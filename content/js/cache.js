7aaa54db2370309e7cc8e5cc7f69e66f029e3b26
thrio <zeus1447@gmail.com>
2019-12-16 01:46:42 +0900

    improve cache.js

--- content/js/cache.js
+++ content/js/cache.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var path = require('path');
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+var cachepath = ["C:/Users/"+userName+"/AppData/LocalLow/VRChat/VRChat/VRCHTTPCache",
+                  "C:/Users/"+userName+"/AppData/LocalLow/VRChat/VRChat/HTTPCache-WindowsPlayer"];
+var cacheFileStats = [];
 
 
 $(function(){
-    let userName = process.env['USERPROFILE'].split(path.sep)[2];
-    let cachepath = "C:/Users/"+userName+"/AppData/LocalLow/VRChat/VRChat/VRCHTTPCache"
-    let totalSize = 0;
-    
+    // ドロップダウンリスト設定
     for(let i=0; i<24; i++){
         $("#hour").append("<option value="+i+">"+i+"時</option>");
     }
@@ -14,44 +15,69 @@ $(function(){
         $("#minute").append("<option value="+i+">"+i+"分</option>");
     }
 
-    // フォルダ内のファイル名を取得
-    fs.readdir(cachepath, (err, files) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        
-        // ファイルだけでループしてトータルサイズ取得
-        for (const file of files) {
-            const fp = path.join(cachepath, file);
-            fs.stat(fp, (err, stats) => {
-                if (err) {
-                    console.error(err);
-                    return;   
-                }
-                if (!stats.isDirectory()) {
-                    totalSize += stats.size;
-                    if(String(totalSize).length > 9){
-                        // GB表示
-                        let dispSize = totalSize/1024**3;
-                        dispSize = String(dispSize.toFixed(2));
-                        $("#total-size").text(dispSize);
-                    }else{
-                        // GB表示
-                        let dispSize = totalSize/1024**2;
-                        dispSize = String(dispSize.toFixed(2));
-                        $("#total-size").text(dispSize);
-                    }
-                    
-                }
-            });
-        }
-        $("#total-count").text(files.length);
-    });
-    
-    
+    // キャッシュ情報セット    
+    setCacheInfo("#total-count", "#total-size");
+
+});
+
+$("#clear-now").on("click", function(){
+    let limitDay = $("#limit-day").val();
+    l(limitDay);
 });
 
 $("#cache-schedule-info").on("click", function(){
     
 });
+
+
+function setCacheInfo(countSel, sizeSel){
+    let totalSize=0;
+    let totalCount=0;
+
+    // フォルダ内のファイル名を取得
+    for(let i=0; i<cachepath.length; i++){
+        if(path.isAbsolute(cachepath[i])){
+            fs.readdir(cachepath[i], (err, files) => {
+                if (err) setErr;
+
+                totalCount += files.length;
+                // ファイルだけでループしてトータルサイズ取得
+                for (let file of files) {
+                    let fp = path.join(cachepath[i], file);
+                    fs.stat(fp, (err, stats) => {
+                        if (err) setErr;
+
+                        if (!stats.isDirectory()) {
+                            totalSize += stats.size;
+                            stats.name = fp;
+                            cacheFileStats.push(stats);
+                        }
+
+                        if(String(totalSize).length > 9){
+                            // GB表示
+                            dispSize = (totalSize/1024**3).toFixed(2);
+                            $(sizeSel).text(dispSize+"GB");
+                        }else{
+                            // MB表示
+                            dispSize = (totalSize/1024**2).toFixed(2);
+                            $(sizeSel).text(dispSize+"MB");
+                        }
+                        //l(typeof(stats.atime));
+                    });                    
+                }
+                $(countSel).text(totalCount);
+            });
+        }else{
+            error = "args["+i+"] is not absolute path.";
+        }
+    }
+
+    function setErr(){
+        $(countSel).text("read error");
+        $(sizeSel).text("read error");
+        return;
+    }
+
+}
+
+function l(arg) {console.log(arg)}
